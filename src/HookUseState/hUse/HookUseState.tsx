086dff8ab6c442540c00a484da0539f958927422
Vladimir Noutbook <vovirus2@inbox.ru>
2025-03-24 13:14:54 +0300

    compare

--- src/HookUseState/hUse/HookUseState.tsx
+++ src/HookUseState/hUse/HookUseState.tsx
@@ -1,31 +1,39 @@
 import { useState } from "react";
 
-export const HookUseState = ()=>{
+export const HookUseState = () => {
+    const [showClicks, setShowClicks] = useState(false);
+    const [clicks, setClicks] = useState(0);
+    const [clicks2, setClicks2] = useState(0);
 
-const [clicks,setClicks]=useState(0);
-const [showClicks,setShowClicks]=useState(false);
-
-
-const onClick = ()=>{
+  const onClick = () => {
     setTimeout(() => {
-        setClicks((prev)=>prev+1)
-        
+      setClicks((prev) => prev + 1);
     }, 1000);
-} 
+  };
+  const onClick2 = () => {
+    setTimeout(() => {
+        setClicks2(clicks2 + 1);
+    }, 1000);
+  };
 
+  const toogleShowClicks = () => {
+    setShowClicks((prev) => !prev);
+  };
+  const clicksText: string | number = showClicks ? clicks : "";
+  const clicksText2: string | number = showClicks ? clicks2 : "";
 
-const toogleShowClicks = ()=>{
-    setShowClicks(prev => !prev)
-}
-const clicksText:string|number = showClicks ? clicks : '';
+  return (
+    <div>
+      <button onClick={onClick}>
+        <span>Click! (function ) {clicksText}</span>
+      </button>
+      <button onClick={onClick2}>
+        <span>Click! (no function ) {clicksText2}</span>
+      </button>
+      <button onClick={toogleShowClicks}>Toogle show clicks</button>
+    </div>
+  );
+};
 
-    return (
-        <div>
-
-        <button onClick={onClick}>
-            <span>Click! {clicksText}</span>
-        </button>
-            <button onClick={toogleShowClicks}>Toogle show clicks</button>
-        </div>
-    )
-}
+//Обратить внимание на передачу состояния в функции Set - в новое состояние лучше передавать функцию -  prev это текущее состояние .
+// setTimeout показывает что при передаче cb состояние всегда будут актуальное
