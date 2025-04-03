99d19bf6a78b76105eb897f18732d6eb4e4948d2
Vladimir Noutbook <vovirus2@inbox.ru>
2025-04-03 10:22:30 +0300

    useElementSize

---
+++ src/CastomHooks/useElementSize/useElementSize.ts
@@ -0,0 +1,31 @@
+import { RefObject, useCallback, useEffect, useState } from 'react';
+import { useEventListener } from '../useEventListener/useEventListener';
+
+type SizeType = {
+    width: number;
+    height: number;
+};
+
+const INIT_SIZE = {
+    width: 0,
+    height: 0,
+};
+
+export const useElementSize = (itemRef: RefObject<HTMLElement>) => {
+    const [size, setSize] = useState<SizeType>(INIT_SIZE);
+
+    const unpdateSizeElement = useCallback(() => {
+        const elemtenRef = itemRef.current;
+        if (elemtenRef) {
+            const { width, height } = elemtenRef.getBoundingClientRect();
+            setSize({ width, height });
+        }
+    }, [itemRef]);
+
+    useEffect(() => {
+        unpdateSizeElement();
+    }, [unpdateSizeElement]);
+    useEventListener('resize', unpdateSizeElement);
+
+    return size;
+};
